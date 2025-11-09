a46eabad65f61dfc0d46f38593f04435c5384d72
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-11-08 21:16:22 -0800

    Updates

---
+++ _next/static/OvScbtvuKyJzcfxrkeJEW/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/scrimba-tutorial-printforge//_next/:path+",
+        "destination": "/scrimba-tutorial-printforge/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
